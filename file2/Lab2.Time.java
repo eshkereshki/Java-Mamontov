70134532157510a9de0405ea3da913a1a73047d1
kefteme <chimichanga1999@mail.ru>
2026-01-18 19:31:42 +0500

    Lab2.Time.java

--- file2/Lab2.Time.java
+++ file2/Lab2.Time.java
@@ -1,12 +1,26 @@
 ### Time.java
 ```java
+/**
+ * Класс Time предназначен для хранения времени,
+ * заданного в секундах, и преобразования его
+ * в формат часы:минуты:секунды.
+ */
 class Time {
+
+    /** Количество секунд */
     private int seconds;
 
+    /**
+     * Создаёт объект времени на основе количества секунд.
+     */
     public Time(int seconds) {
         this.seconds = seconds;
     }
 
+    /**
+     * Преобразует время в текстовый формат ЧЧ:ММ:СС.
+     * Учитывает переполнение суток.
+     */
     @Override
     public String toString() {
         int totalSeconds = seconds % (24 * 3600);
@@ -17,6 +31,10 @@ class Time {
         return String.format("%d:%02d:%02d", hours, minutes, secs);
     }
 
+    /**
+     * Демонстрационный метод.
+     * Показывает преобразование секунд в формат времени.
+     */
     public static void demo() {
         System.out.println("\n Время");
 
@@ -24,9 +42,10 @@ class Time {
         Time time2 = new Time(10000);
         Time time3 = new Time(100000);
 
-        System.out.println("10 секунд: " + time1.toString());
-        System.out.println("10000 секунд: " + time2.toString());
-        System.out.println("100000 секунд: " + time3.toString());
+        System.out.println("10 секунд: " + time1);
+        System.out.println("10000 секунд: " + time2);
+        System.out.println("100000 секунд: " + time3);
     }
 }
+
 ```
