e08d2e5fbf20ac88228d4717cf7a7b962f9c7869
kefteme <chimichanga1999@mail.ru>
2026-03-09 17:27:06 +0500

    Add TwoAnnotatedClass with @Two annotation

---
+++ file6/5/TwoAnnotatedClass.java
@@ -0,0 +1,11 @@
+/**
+ * Класс для демонстрации аннотации @Two.
+ */
+@Two(first = "Пример строки", second = 42)
+public class TwoAnnotatedClass {
+
+    /**
+     * Пример поля.
+     */
+    private String example;
+}
