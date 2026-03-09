33584a62432c7230c1ba4078f5c5a92d6b5fd32c
kefteme <chimichanga1999@mail.ru>
2026-03-09 17:17:28 +0500

    Fix annotation formatting in DefaultAnnotatedClass

---
+++ file6/2/DefaultAnnotatedClass.java
@@ -0,0 +1,11 @@
+/**
+ * Класс, демонстрирующий использование аннотации @Default.
+ */
+@Default(String.class) 
+public class DefaultAnnotatedClass {
+
+    /**
+     * Пример поля класса.
+     */
+    private String exampleField;
+}
